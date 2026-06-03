63bb60c9633bb2aab9b420e0d3e8cafb604e4cb1
Dinesh-2026 <dineshsanthosh6@gmail.com>
2026-06-03 20:36:44 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -27,3 +27,4 @@ search_tool = Tool(
 
 api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
 wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
+print("succesfully completed ")
